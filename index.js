cfc888670501182aab57b95c94c0b43484dfab50
amore-ibm <122065191+amore-ibm@users.noreply.github.com>
2023-02-20 16:58:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require('path');
 
-//JSON blocks to be returned
-var bluerose = { 'identifiers': { 'color': 'blue', 'flowertype': 'rose' }, 'instock': 'true', 'number': '22', 'cost': '10.32', 'hours': '3', 'key': 'match' };
-var bluedaisy = { 'identifiers': { 'color': 'blue', 'flowertype': 'daisy' }, 'instock': 'true', 'number': '43', 'cost': '7.22', 'hours': '0.5', 'key': 'match' };
-var redrose = { 'identifiers': { 'color': 'red', 'flowertype': 'rose' }, 'instock': 'true', 'number': '32', 'cost': '11.57', 'hours': '1.5', 'key': 'match' };
-var reddaisy = { 'identifiers': { 'color': 'red', 'flowertype': 'daisy' }, 'instock': 'true', 'number': '12', 'cost': '5.49', 'hours': '2', 'key': 'match' };
-var whitetulip = { 'identifiers': { 'color': 'white', 'flowertype': 'tulip' }, 'instock': 'true', 'number': '34', 'cost': '2.40', 'hours': '1', 'key': 'match' };
-
 //port setup
 const PORT = 8080;
 
@@ -16,86 +9,45 @@ const PORT = 8080;
 const app = express();
 app.use(bodyParser.json());
 
+// Data Science knowledge base
+const data_science_kb = {
+    "Data Engineering": {
+        "Child": "Data engineering is like building a big toy castle, but instead of using toy blocks, you use a lot of numbers and letters to make sure the castle is strong and can be played with easily.",
+        "High School Student": "Data engineering is like building a huge puzzle with many different pieces that need to fit together perfectly. The pieces in this case are a lot of data, and data engineers make sure that the pieces are organized and can be used to solve problems and answer questions.",
+        "College Student": "Data engineering is the process of designing, building, and maintaining the infrastructure that allows data scientists and analysts to work with large amounts of data. This includes developing systems to store, process, and transform data so that it can be used for analysis and decision-making.",
+        "Expert": "Data engineering involves the use of various techniques and tools to manage and optimize large-scale data processing systems. It encompasses the design and implementation of data pipelines, ETL (extract, transform, load) processes, data warehouses, and other systems to enable efficient and scalable data analysis. Data engineers also work closely with data scientists and analysts to ensure that the data they use is accurate, consistent, and properly stored."
+        },
+    "Machine Learning": {
+        "Child": "Machine learning is like teaching your toy robot how to make decisions. You give it a lot of examples and it learns from them, so it can make good choices on its own.",
+        "High School Student": "Machine learning is a way to teach computers how to learn from data, without being explicitly programmed. By using algorithms and statistical models, computers can identify patterns and make predictions based on data.",
+        "College Student": "Machine learning is a subset of artificial intelligence that focuses on building algorithms and models that enable computers to automatically improve their performance on a specific task by learning from data. This involves selecting appropriate features, creating and training models, and evaluating their performance.",
+        "Expert": "Machine learning involves the development of complex algorithms and models that can identify patterns and relationships in large datasets. It involves selecting and preparing data, selecting appropriate models and algorithms, tuning their parameters, and evaluating their performance. Experts in machine learning use a variety of techniques such as deep learning, reinforcement learning, and natural language processing to build advanced models that can be used in a wide range of applications." 
+        },
+    "Data Visualization": {
+        "Child": "Data visualization is like drawing pictures with numbers. You can use different colors and shapes to show the important parts of the data, and make it easier to understand.",
+        "High School Student": "Data visualization is a way to present data in a graphical or visual format, such as charts, graphs, and maps. By representing data visually, it can be easier to understand trends, patterns, and relationships in the data.",
+        "College Student": "Data visualization involves the creation of effective and informative visual representations of data. This includes selecting appropriate visual encodings, designing visualizations to match the message, and understanding how to use visualizations to communicate insights and findings.",
+        "Expert": "Data visualization is the process of creating visual representations of data to help people better understand, analyze, and communicate information. Experts in data visualization use a variety of techniques, such as information design, visual perception, and human-computer interaction, to create effective visualizations that are easy to interpret and communicate complex ideas." 
+        }  
+  };
 
-//Endpoint handleing
-app.post('/flowerstock', function (req, res) {//check the flowers we have in stock
-    //console.log("Flowerstock hit"); //debug
-    var resp = { "key": "No_match" };//If there's no matching if statement, default to body: no_match
-    //console.log(req); //debug
-    var b0ddy = req["body"]
-    var color = b0ddy["color"];
-    var type = b0ddy["flowertype"];
+// Handle requests to the API
+app.post('/knowledge', function (req, res) {
+    // Retrieve arguments from request body
+    const topic = req.body.topic;
+    const level = req.body.level;
+    
+    // Look up response in knowledge base
+    const response = data_science_kb[topic][level];
+  
+    // Send response back
+    res.send({ response: response });
+  });
 
-    //if loops
-    if (color.toLowerCase() == "blue" && (type.toLowerCase() == "rose" || type.toLowerCase() == 'roses')) {
-        resp = bluerose;
-    }
-    if (color.toLowerCase() == "blue" && (type.toLowerCase() == "daisy" ||
-        type.toLowerCase() == 'daisies' || type.toLowerCase() == 'daisys')) {
-        resp = bluedaisy;
-    }
-    if (color.toLowerCase() == "red" && (type.toLowerCase() == "rose" || type.toLowerCase() == 'roses')) {
-        resp = redrose;
-    }
-    if (color.toLowerCase() == "red" && (type.toLowerCase() == "daisy" ||
-        type.toLowerCase() == 'daisies' || type.toLowerCase() == 'daisys')) {
-        resp = reddaisy;
-    }
-    if (color.toLowerCase() == "white" && (type.toLowerCase() == "tulip" || type.toLowerCase() == 'tulips')) {
-        resp = whitetulip;
-    }
-
-    //send the response back
-    res.send(resp);
-});
-
-//post endpoint for odering
-app.post('/order', (req, res) => {
-    var resp = { "key": "No_match" };//If there's no matching if statement, default to body: no_match
-    var b0dy = req["body"];
-    var color = b0dy["color"];
-    var type = b0dy["flowertype"];
-    var number = parseFloat(b0dy['number']); //how many flowers were ordered
-    console.log(number);
-
-    //if loops
-    if (color.toLowerCase() == "blue" && (type.toLowerCase() == "rose" || type.toLowerCase() == 'roses')) {
-        var totals = (parseFloat(bluerose['cost']) * number);
-        resp = { 'total': totals, 'message': 'Thankyou for your order', 'key': 'match' };
-    }
-    if (color.toLowerCase() == "blue" && (type.toLowerCase() == "daisy" ||
-        type.toLowerCase() == 'daisies' || type.toLowerCase() == 'daisys')) {
-        var totals = (parseFloat(bluedaisy['cost']) * number);
-        resp = { 'total': totals, 'message': 'Thankyou for your order', 'key': 'match' };
-    }
-    if (color.toLowerCase() == "red" && (type.toLowerCase() == "rose" || type.toLowerCase() == 'roses')) {
-        var totals = (parseFloat(redrose['cost']) * number);
-        resp = { 'total': totals, 'message': 'Thankyou for your order', 'key': 'match' };
-    }
-    if (color.toLowerCase() == "red" && (type.toLowerCase() == "daisy" ||
-        type.toLowerCase() == 'daisies' || type.toLowerCase() == 'daisys')) {
-        var totals = (parseFloat(reddaisy['cost']) * number);
-        resp = { 'total': totals, 'message': 'Thankyou for your order', 'key': 'match' };
-    }
-    if (color.toLowerCase() == "white" && (type.toLowerCase() == "tulip" || type.toLowerCase() == 'tulips')) {
-        var totals = (parseFloat(whitetulip['cost']) * number);
-        resp = { 'total': totals, 'message': 'Thankyou for your order', 'key': 'match' };
-    }
-
-    //send the resoonse back 
-    res.send(resp);
-});
-
-//endpoint for testing the status of the API 
-app.get('/status', (req, res) => {
-    const somee = { msg: 'success', status: 'okay' }
-    res.send(somee);
-})
-
+// Serve homepage
 app.get('', (req, res) => {
-    //return the html file to be rendered
-    res.sendFile(path.join(__dirname, '/index.html'));
-});
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
 
 app.listen(PORT, () => {
     console.log("Server running on port 8080");
